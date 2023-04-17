59eae8cee773c1171d79f2d61a3671c68d48b419
Emerson Marinho <Emerson_marinhoaz@hotmail.com>
2023-04-17 18:43:55 -0300

    feat: req 07 Added the table and priting the values of which the user set into the input and also used the global state to get the expenses information

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
@@ -18,10 +21,56 @@ class Table extends Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {expenses.map((item) => (
+              <tr key={ item.id }>
+                <td>{item.description}</td>
+                <td>{item.tag}</td>
+                <td>{item.method}</td>
+                <td>{Number(item.value).toFixed(2)}</td>
+                <td>{item.currency}</td>
+                <td>{item.exchangeRates[item.currency].name}</td>
+                <td>{Number(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+                <td>
+                  {`R$ ${(item.value * item.exchangeRates[item.currency]
+                    .ask).toFixed(2)}`}
+                </td>
+                <td>Real</td>
+                <td>Editar/Excluir</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired,
+      currency: PropTypes.string.isRequired,
+      method: PropTypes.string.isRequired,
+      tag: PropTypes.string.isRequired,
+      exchangeRates: PropTypes.shape({
+        USD: PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          ask: PropTypes.number.isRequired,
+          bid: PropTypes.number.isRequired,
+          code: PropTypes.string.isRequired,
+          create_at: PropTypes.string.isRequired,
+          update_at: PropTypes.string.isRequired,
+        }),
+      }).isRequired,
+    }),
+  ).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
